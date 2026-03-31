6d6de403f0f29103a281549b1523fd38fc3a8395
basmala004 <basmala0404@gmail.com>
2026-03-31 21:28:44 +0200

    12

---
+++ Task-12/project/lib/screens/archive_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class ArchiveScreen extends StatefulWidget {
+  const ArchiveScreen({super.key});
+
+  @override
+  State<ArchiveScreen> createState() => _ArchiveScreenState();
+}
+
+class _ArchiveScreenState extends State<ArchiveScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Archive Screen'),
+    );
+  }
+}
